504387fc6f43d8d90a57f8d259e78e485774b1a6
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-05-14 12:24:01 +0200

    test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,9 +4,10 @@ const { test, expect } = require('@playwright/test');
 test('test', async ({ page }) => {
   await page.goto('https://www.wikipedia.org/');
   await page.getByRole('link', { name: 'English 6,796,000+ articles' }).click();
-  await page.getByText('Mars', { exact: true }).click();
-  await page.getByText('Mars is the fourth planet').click();
-  await page.getByText('Mars was formed approximately').click();
-  await page.getByRole('link', { name: 'Late Heavy Bombardment', exact: true }).click();
+  await page.getByRole('link', { name: 'Frank Bainimarama' }).click();
+  await page.getByText('Fiji coup of 2000', { exact: true }).click();
+  await page.getByText('Post-2000 coup', { exact: true }).click();
+  await page.getByRole('link', { name: '2006 Fijian coup d\'état' }).click();
+  await page.getByRole('heading', { name: 'Fijian coup d\'état' }).locator('span').click();
   await page.screenshot({path: `wiki_screen.png`});
 });
